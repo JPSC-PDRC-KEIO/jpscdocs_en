53b730097818ea066f9acd4a2a2b33b84ab616ba
JPSC-PDRC-KEIO <sakaguchi@DESKTOP-849RB95.localdomain>
2020-03-11 20:02:39 +0900

    change the author name in the TeX and PDF documents

--- conf.py
+++ conf.py
@@ -268,7 +268,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, "jpscdoc.tex", "jpscdoc Documentation", "SAKAGUCHI Naofumi", "manual"),
+    (master_doc, "jpscdoc.tex", "JPSC Questionnaire", "JPSC-PDRC-KEIO", "manual"),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
